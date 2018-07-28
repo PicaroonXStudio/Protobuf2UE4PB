953604968a835e2e6bb74b0ab727e892d8f271d9
Alan Liu <alan.liuhongliang@gmail.com>
2018-07-28 10:47:33 +0800

    #pragma warning(disable : 4668)

--- cpp_file.cc
+++ cpp_file.cc
@@ -977,13 +977,14 @@ void FileGenerator::GenerateTopHeaderGuard(io::Printer* printer,
 		"#if defined(_MSC_VER) && _MSC_VER >= 1300" "\n"
 		"#ifndef _PS3" "\n"
 		"#pragma warning(push)" "\n"
-		"#pragma warning(disable : 4530)" "\n"  // warning C4530: C++ exception handler used, but unwind semantics are not enabled. Specify /EHsc (disabled due to std headers having exception syntax)"
-		"#pragma warning(disable : 4512)" "\n"  // warning C4512: assignment operator could not be generated
-		"#pragma warning(disable : 4244)" "\n"  // warning C4244:  warning C4244: '=' : conversion from '__w64 int' to 'int', possible loss of data
-		"#pragma warning(disable : 4267)" "\n"  // warning C4267: 'argument' : conversion from 'size_t' to 'int', possible loss of data
-		"#pragma warning(disable : 4125)" "\n" // warning C4125: decimal digit terminates octal escape sequence
-		"#pragma warning(disable : 4127)" "\n" // warning C4127: conditional expression is constant
-		"#pragma warning(disable : 4100)" "\n"  // warning C4100: 'op' : unreferenced formal parameter
+		"#pragma warning(disable : 4530)" "\n"
+		"#pragma warning(disable : 4512)" "\n"
+		"#pragma warning(disable : 4244)" "\n"
+		"#pragma warning(disable : 4267)" "\n"
+		"#pragma warning(disable : 4125)" "\n"
+		"#pragma warning(disable : 4127)" "\n"
+		"#pragma warning(disable : 4100)" "\n"
+		"#pragma warning(disable : 4668)" "\n"
 		"#endif" "\n" // _PS3
 		"#endif" "\n" // _MSC_VER
 
